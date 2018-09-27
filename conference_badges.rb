200960bf15d334992d7a91c643dab077835313ad
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-27 20:26:55 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,6 @@ def assign_rooms(speakers)
   assignments = []
   speakers.each do |name|
     assignments << "Hello, #{name}! You'll be assigned to room #{speakers.index(name)}!"
-    end
   end
 end
 
