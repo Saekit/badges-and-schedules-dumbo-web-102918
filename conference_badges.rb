773ff32b10ba89bb906fda2ac10a4bd041674d20
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-27 19:55:23 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  batch = []
+  speakers.each do |name|
+    batch << "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms(speakers)
+  
+end
+
+def printer
+  batch_badge_creator(speakers)
+  assign_rooms(speakers)
+end
